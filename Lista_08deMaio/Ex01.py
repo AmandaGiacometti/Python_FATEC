0d1bcaef3b798e4bac047549c533465c8418c58e
Amanda Giacometti <amandagiacomettipedrosa@gmail.com>
2023-05-08 16:04:54 -0300

    Exercício 01

---
+++ Lista_08deMaio/Ex01.py
@@ -0,0 +1,19 @@
+#Elabore um algoritmo que leia três números pelo teclado, coloque-os em ordem crescente (N<=N2<=N3)
+#e mostre-os em ordem crescente no monitor.
+
+num1 = input("Digite o primeiro número: ")
+num2 = input("Digite o segundo número: ")
+num3 = input("Digite o terceiro número: ")
+
+if num1 <= num2 <= num3:
+    print("Em ordem crescente: ",num1,", ",num2,", ",num3)
+elif num1 <= num3 <= num2:
+    print("Em ordem crescente: ",num1,", ",num3,", ",num2)
+elif num2 <= num1 <= num3:
+    print("Em ordem crescente: ",num2,", ",num1,", ",num3)
+elif num2 <= num3 <= num1:
+    print("Em ordem crescente: ",num2,", ",num3,", ",num1)
+elif num3 <= num1 <= num2:
+    print("Em ordem crescente: ",num3,", ",num1,", ",num2)
+else:
+    print("Em ordem crescente: ", num3, ", ", num2, ", ", num1)
